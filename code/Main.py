736f22d873bfdc081d74c7bba127ceec24389560
甘俊浩 <71586668+33957024@users.noreply.github.com>
2022-07-07 11:49:19 +0800

    Delete Main.py

--- code/Main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from Mainset import mainwindows
-from PyQt5.QtWidgets import QApplication
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = mainwindows()
-    w.show()
-    sys.exit(app.exec_())
