7057de7bcf812986c146ba0dfab6e6572960fe32
33957024 <71586668+33957024@users.noreply.github.com>
2022-09-11 19:06:00 +0800

    updata

--- read_price.py
+++ read_price.py
@@ -1,5 +1,8 @@
+import traceback
+
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
 from PyQt5.uic import loadUi
+from PyQt5.QtCore import QStringListModel
 from PyQt5.QtGui import QFontDatabase
 from sys import argv, exit
 from xml.etree.ElementTree import (
@@ -10,27 +13,52 @@ from xml.etree.ElementTree import (
 class MainWindows(QMainWindow):
     def __init__(self):
         # 自定义字体导入
+        self.identifier = []
+        self.pricelist = []
+        self.path2 = None
         self.path = None
         QFontDatabase.addApplicationFont('./Font/Bender_Light.otf')
         super(MainWindows, self).__init__()
         loadUi('UI/read.ui', self)
 
     def file(self):
+        self.pricelist.clear()
         try:
             tree = ElementTree(file=self.path)
+            tree2 = ElementTree(file=self.path2)
             a = tree.findall('./Item')
             for path in a:
+                self.identifier = path.get('identifier')
+                b = 'NULL'
                 d = path.get('name')
-                print('名字:' + d)
-                b = path.find('Price').attrib.get('baseprice')
-                print('价格:' + b)
+
+                try:
+                    b = path.find('Price').attrib.get('baseprice')
+                except:
+                    pass
+                try:
+                    d1 = tree2.find('entityname.' + self.identifier).text
+                    b1 = tree2.find('entitydescription.' + self.identifier).text
+                    name_lang = '翻译名字' + d1 + '翻译简介' + b1
+                    name_file = '名字: ' + d + ' 价格: ' + b + '标识符' + path.get('identifier')
+                    c = name_file + name_lang
+                    self.pricelist.append(c)
+                except:
+                    pass
+
         except:
+            print(traceback.print_exc())
             QMessageBox.warning(self, '错误', '路径或文件有问题捏')
+        slm = QStringListModel()
+        slm.setStringList(self.pricelist)
+        self.listView.setModel(slm)
 
     def read(self):
         path, file = QFileDialog.getOpenFileName(self, '选择文件', 'C:/', '物品XML-files(*.xml)')
         self.path = path
-        QMessageBox.information(self, '确认路径', path)
+        path2, file2 = QFileDialog.getOpenFileName(self, '选择语言', path, '语言XML-files(*.XML)')
+        self.path2 = path2
+        QMessageBox.information(self, '确认路径', '文件路径' + path + '\n语言路径:\n' + path2)
 
 
 if __name__ == '__main__':
